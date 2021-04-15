d3a50b7f91b56b5cd19d34f6fe1e3652682e5e05
alinaHinzhulBSNU <alinahinzhul@gmail.com>
2021-04-15 19:42:22 +0300

    DEP-63 Add converter from marks to ECTS for grade

--- app/Models/Grade.php
+++ app/Models/Grade.php
@@ -44,4 +44,33 @@ class Grade extends Model
         
         return $result;
     }
+
+    // Переведення в оцінку за шкалою ECTS
+    public function toECTS(){
+        $result = "ECTS";
+
+        if(0 <= $this->grade and $this->grade <= 34){
+            $result = "F";
+        }
+        else if(35 <= $this->grade and $this->grade <= 59){
+            $result = "FX";
+        }
+        else if(60 <= $this->grade and $this->grade <= 66){
+            $result = "E";
+        }
+        else if(67 <= $this->grade and $this->grade <= 74){
+            $result = "D";
+        }
+        else if(75 <= $this->grade and $this->grade <= 81){
+            $result = "C";
+        }
+        else if(82 <= $this->grade and $this->grade <= 89){
+            $result = "B";
+        }
+        else if(90 <= $this->grade and $this->grade <= 100){
+            $result = "A";
+        }
+
+        return $result;
+    }
 }
