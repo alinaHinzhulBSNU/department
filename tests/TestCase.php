7fee0254b0ce3199850b7021b9c7365379d03cbf
alinaHinzhulBSNU <alinahinzhul@gmail.com>
2021-04-26 11:00:06 +0300

    Change TestCase.php file

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -4,12 +4,6 @@ namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
 
-abstract class TestCase extends BaseTestCase
-{
+abstract class TestCase extends BaseTestCase{
     use CreatesApplication;
-
-    public function setUp():void
-    {
-        parent::setUp();
-    }
 }
